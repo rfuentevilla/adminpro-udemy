b9e674c17223a038b2db0f10535b0495cc4b75f1
Richard Fuentevilla <rfuentevillar@gmail.com>
2019-09-25 15:40:27 -0300

    Seccion 8 Terminada

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -21,6 +21,8 @@ import { ChartsModule } from 'ng2-charts';
 
 // setting del usuario
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 @NgModule({
     declarations: [
@@ -30,7 +32,9 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
         Graficas1Component,
         IncrementadorComponent,
         GarficaDonaComponent,
-        AccountSettingsComponent
+        AccountSettingsComponent,
+        PromesasComponent,
+        RxjsComponent
     ],
     exports: [
         PagesComponent,
--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -5,6 +5,8 @@ import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 
 const pagesRoutes: Routes = [
@@ -12,10 +14,12 @@ const pagesRoutes: Routes = [
         path: '',
         component: PagesComponent,
         children: [
-            { path: 'dashboard', component: DashboardComponent },
-            { path: 'progress', component: ProgressComponent },
-            { path: 'graficas1', component: Graficas1Component },
-            { path: 'account-setting', component: AccountSettingsComponent },
+            { path: 'dashboard', component: DashboardComponent, data: { titulo: 'Dashboard' } },
+            { path: 'progress', component: ProgressComponent, data: { titulo: 'Progress' }  },
+            { path: 'graficas1', component: Graficas1Component, data: { titulo: 'Graficas' }  },
+            { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' }  },
+            { path: 'rxjs', component: RxjsComponent, data: { titulo: 'RxJs' }  },
+            { path: 'account-setting', component: AccountSettingsComponent, data: { titulo: 'Ajustes del Tema' }  },
             { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
         ]
     },
